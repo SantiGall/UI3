4c430177efc7ba93e9abfa423e66b1a1c666c3d8
SantiGall <164009868+SantiGall@users.noreply.github.com>
2024-05-28 08:53:15 -0300

    Password Codeada

---
+++ Assets/Password.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Password : MonoBehaviour
+{
+    public string password;
+    public InputField input;
+
+    public void test()
+    {
+        if(input.text == password)
+        {
+            Debug.Log("Access Granted");
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
+    }
+    
+}
